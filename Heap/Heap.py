d0802aea7bceb1b9a505130cb9639763f1f5350e
Wuhao Chen <c.jim8889@outlook.com>
2019-02-24 19:56:56 +0000

    complete max heap

--- Heap/Heap.py
+++ Heap/Heap.py
@@ -1,13 +1,11 @@
 
 
 class Heap:
-
-    def __init__(self, rootValue):
+    # Max heap
+    def __init__(self):
 
         self.dictionary = dict()
-        self.dictionary[0] = rootValue
-        self.size = 1
-
+        self.size = 0
     
     def exchange(self, i, j):
 
@@ -31,13 +29,45 @@ class Heap:
         self.swim(self.size)
         self.size += 1
 
+    def sink(self, i):
+
+        while (2 * i + 1 < self.size):
+            j = 2 * i + 1
+
+            if (j < self.size - 1 and self.less(j, j + 1)):
+                j += 1
+            
+            if (not self.less(i, j)):
+                break
+            
+            self.exchange(i, j)
+            i = j
+
+    def popMax(self):
+        
+        if (not self.size > 0):
+            return
+
+        max = self.dictionary[0]
+        self.exchange(0, self.size - 1)
+        self.size -= 1
+        del self.dictionary[self.size]
+        self.sink(0)
+
+        return max
+
     
         
 
 if __name__ == "__main__":
-    heap = Heap(5)
+    heap = Heap()
     heap.insert(8)
     heap.insert(4)
     heap.insert(20)
     heap.insert(92)
     print(heap.dictionary)
+
+
+    while (heap.size > 0):
+        
+        print(heap.popMax())
